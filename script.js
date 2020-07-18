cbe846855a8f62177ca90d0b0fa1e820f6be269f
soeun <70000city@gmail.com>
2020-07-19 02:37:37 +0900

    3

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
 "use strict";
 
-function addRow() {
-  let tbody = document.getElementById("mytbody");
-  let newRow = tbody.insertRow(0);
-  let newCell = newRow.insertCell(0);
+let posting = document.querySelectorAll(".box");
 
-  newCell.innerHTML = "새 게시글입니다";
+let title = () => {
+  return posting[0].querySelector("input").value;
+};
+
+let fee = () => {
+  let checked = posting[1].querySelectorAll("label>input");
+  for (let i = 0; i < 2; i++) {
+    if (checked[i].checked) {
+      return checked[i].value;
+    }
+  }
+};
+
+let city = () => {
+  let citySelect = posting[2].querySelector("select");
+  return citySelect.options[citySelect.selectedIndex].text;
+};
+
+let position = () => {
+  let checkCount = posting[3].querySelectorAll("input").length;
+  let positionList = new Array();
+
+  for (let i = 0; i < checkCount; i++) {
+    if (posting[3].querySelectorAll("input")[i].checked == true) {
+      positionList.push(posting[3].querySelectorAll("input")[i].value);
+    }
+  }
+  return positionList;
+};
+
+function getPosting() {
+  title();
+  fee();
+  city();
+  position();
+  console.log(title(), fee(), city(), position());
 }
 
-addRow();
+function addRow() {
+  let titleText = title();
+  let positionText = position();
+  let feeText = fee();
+  let cityText = city();
+
+  let tbody = document.body.querySelector("tbody");
+
+  if (tbody) {
+    let newRow = tbody.insertRow(0);
+
+    let newTitle = newRow.insertCell(0);
+    newTitle.setAttribute("class", "title");
+    newTitle.innerHTML = titleText;
+
+    let newPosition = newRow.insertCell(1);
+    newPosition.innerHTML = positionText;
+
+    let newCity = newRow.insertCell(2);
+    newCity.innerHTML = cityText;
+
+    let newFee = newRow.insertCell(3);
+    newFee.innerHTML = feeText;
+  }
+}
